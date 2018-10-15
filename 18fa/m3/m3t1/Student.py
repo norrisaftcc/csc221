c978e7ed0dd7c8eb9686dac37260365fa8cdeb22
Andrew Norris <norrisa@faytechcc.edu>
2018-10-15 13:20:35 -0400

    Initial commit of Student class

---
+++ 18fa/m3/m3t1/Student.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+The Student class.
+"""
+
+class Student(object):
+    
+    # constructor
+    def __init__(self, id, firstName, lastName, program, gpa):
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.program = program
+        self.gpa = gpa
+        
+        
+    
+
