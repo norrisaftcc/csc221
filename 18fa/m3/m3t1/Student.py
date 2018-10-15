39710e4a18f2cce812356b1054bb05a68d75691f
Andrew Norris <norrisa@faytechcc.edu>
2018-10-15 13:25:07 -0400

    added str

--- 18fa/m3/m3t1/Student.py
+++ 18fa/m3/m3t1/Student.py
@@ -5,14 +5,20 @@ The Student class.
 
 class Student(object):
     
-    # constructor
+  
     def __init__(self, id, firstName, lastName, program, gpa):
+        """ constructor """
         self.id = id
         self.firstName = firstName
         self.lastName = lastName
         self.program = program
         self.gpa = gpa
         
-        
+    
+    def __str__(self):
+        """ string representation """
+        text = str(self.id) + " " + self.firstName + " " + self.lastName
+        text += " " + self.program + " " + str(self.gpa)
+        return text
     
 
