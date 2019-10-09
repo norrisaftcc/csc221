b6c062783e56f2b9edd8dbbd149c6b4092a43872
Andrew Norris <norrisa@faytechcc.edu>
2019-10-09 14:17:29 -0400

    Added Room IDs

--- 19fa/Adventure/m3lab.py
+++ 19fa/Adventure/m3lab.py
@@ -1,12 +1,17 @@
 from room import Room
 
-kitchen = Room("Kitchen","A kitchen.")
-bedroom = Room("Bedroom","A bedroom.")
+start = Room("The Void","You should never see this room...")
 livingRoom = Room("Living Room","Filled with dust.")
+diningRoom = Room("Dining Room","Filled with dust.")
+hall = Room ("Hall","There is an old clock here.")
+den = Room ("Den","Filled with old sofas.")
+kitchen = Room("Kitchen","A kitchen.")
 
-house = [kitchen, bedroom, livingRoom]
+
+house = [start, livingRoom, diningRoom, hall, den, kitchen]
 
 for room in house:
     print("Moving to a new room...")
     print (room)
+    
     
